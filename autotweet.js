b95f69d5bb248d6feb6a60ee03dd66ff52f057b2
Toby Fee <TobyFee@gmail.com>
2017-06-21 21:40:41 -0700

    moved config values to .env

--- autotweet.js
+++ autotweet.js
@@ -1,5 +1,10 @@
 var twit = require('twit');
-var config = require('./config.js');
+var config = ({
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
 
 
 var Twitter = new twit(config);
