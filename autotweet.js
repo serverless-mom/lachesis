2a9eeef427dc9c9a4ff8285b51ecfbc8b9a03375
Toby Fee <TobyFee@gmail.com>
2017-06-21 22:12:11 -0700

    randomized strings

--- autotweet.js
+++ autotweet.js
@@ -1,15 +1,27 @@
 var twit = require('twit');
+var dictionaries = require('./dictionaries');
 var config = ({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
+function chooseRandom(myArray) {
+  return myArray[Math.floor(Math.random() * myArray.length)];
+}
 
 var Twitter = new twit(config);
 
-Twitter.post('statuses/update', { status: `now I'm putting the the whole thing on Heroku` },
-function(err, data, response) {
-  if (err){console.log(data)};
-})
+var tweetText = ChooseRandom(dictionaries.starts) +
+  ' ' + ChooseRandom(dictionaries.middles) +
+  ' ' + ChooseRandom(dictionaries.endings)
+
+Twitter.post('statuses/update', {
+    status: tweetText
+  },
+  function(err, data, response) {
+    if (err) {
+      console.log(data)
+    };
+  })
---
+++ dictionaries.js
@@ -0,0 +1,24 @@
+var starts = [
+  `It's been a little while,`,
+  `Twitter's fun but enough's enough!`
+  ]
+var middles = [
+  'Log off for a little but.',
+  'Do something else for a bit.',
+  'Time to close twitter.',
+  'Give your notifications time to pile up.',
+  'Do something you enjoy',
+  'Go look at something else.'
+]
+
+var endings = [
+  'I love you.',
+  'Thank you for taking a break.',
+  `I'll tweet again in another hour.`,
+]
+
+module.exports = {
+  starts: starts,
+  middles: middles,
+  endings: endings,
+}
