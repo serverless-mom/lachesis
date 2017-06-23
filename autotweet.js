9c8247493b5ef0e92e05f765c5826c62d561cf4d
Toby Fee <TobyFee@gmail.com>
2017-06-22 23:07:08 -0700

    dictionary updates, and it now deletes the right tweet

--- autotweet.js
+++ autotweet.js
@@ -10,22 +10,35 @@ if (!process.env.BOT_CONSUMER_KEY) {
   config = require('./config');
 }
 
+var numberOfSavedTweets = 5;
+
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
 var Twitter = new twit(config);
 
+function MakeRandomText() {
+  var returnText = chooseRandom(dictionaries.starts) +
+    ' ' + chooseRandom(dictionaries.middles) +
+    ' ' + chooseRandom(dictionaries.endings)
+  if (returnText.length < 140) {
+    return returnText
+  } else {
+    return MakeRandomText()
+  }
+}
+
 //delete the previous tweet
 Twitter.get('statuses/user_timeline', {
     screen_name: 'LogoffBot',
-    count: '1'
+    count: 'numberOfSavedTweets'
   },
   function(err, data, response) {
     if (err) {
       console.log('error!' + err)
-    } else if (data && data[0].id_str) { //check that there really was a tweet to delete
-      var deleteID = data[0].id_str;
+    } else if (data && data[(numberOfSavedTweets - 1)].id_str) { //check that there really was a tweet to delete
+      var deleteID = data[(numberOfSavedTweets - 1)].id_str;
       Twitter.post('statuses/destroy/' + deleteID, {
           'id': deleteID
         },
@@ -37,9 +50,8 @@ Twitter.get('statuses/user_timeline', {
     }
   })
 
-var tweetText = chooseRandom(dictionaries.starts) +
-  ' ' + chooseRandom(dictionaries.middles) +
-  ' ' + chooseRandom(dictionaries.endings)
+var tweetText = MakeRandomText();
+console.log(tweetText);
 
 Twitter.post('statuses/update', {
     status: tweetText
--- dictionaries.js
+++ dictionaries.js
@@ -1,14 +1,18 @@
 var starts = [
   `It's been a little while,`,
   `Twitter's fun but enough's enough!`,
-  `60 minutes have passed`
+  `60 minutes have passed.`,
+  `It's fun to scroll the TL for an hour, 2 hours tho?`,
+  `Ask yourself, is Twitter fun right now?`,
+  `You followed me bc you wanted to change ur patterns.`
   ]
+
 var middles = [
-  'Log off for a little bit.\n\n',
+  'Make sure ur where you want to be.\n\n',
   'Do something else for a bit.',
-  'Time to close the tab, put down the phone, and stand up.',
+  'Time to close the tab/put down the phone.',
   'Give your notifications time to pile up.',
-  'Do something you enjoy!',
+  'Do something you enjoy!\n\n',
   'Stretch or go read or just call a friend!',
   'Go look at something else.'
 ]
@@ -18,6 +22,8 @@ var endings = [
   `Thank you for taking a break.`,
   `I'll tweet again in another hour.`,
   `Don't thank me, YOU followed me.`,
+  `It's not abt being perfect. Do your best`,
+  `RT this to encourage a friend.`
 ]
 
 module.exports = {
