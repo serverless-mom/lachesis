4ba8581fab0295f7b0c4536c1ce83a36cdad6c50
Toby Fee <TobyFee@gmail.com>
2017-08-22 14:05:02 -0700

    returning a random tarot card with no current preference for light or shadow versions. Lots of commented out code for the logoff bot this was based on

--- autotweet.js
+++ autotweet.js
@@ -1,63 +1,37 @@
 var twit = require('twit');
-var dictionaries = require('./dictionaries');
-var config = ({
-  consumer_key: process.env.BOT_CONSUMER_KEY,
-  consumer_secret: process.env.BOT_CONSUMER_SECRET,
-  access_token: process.env.BOT_ACCESS_TOKEN,
-  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
-if (!process.env.BOT_CONSUMER_KEY) {
-  config = require('./config');
-}
+var corpora = require('corpora-project');
+var deck = corpora.getFile('divination', 'tarot_interpretations');
 
-var numberOfSavedTweets = 5;
+// var dictionaries = require('./dictionaries');
+// var config = ({
+//   consumer_key: process.env.BOT_CONSUMER_KEY,
+//   consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//   access_token: process.env.BOT_ACCESS_TOKEN,
+//   access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// });
+// if (!process.env.BOT_CONSUMER_KEY) {
+//   config = require('./config');
+// }
+// var Twitter = new twit(config);
+
 
 function chooseRandom(myArray) {
   return myArray[Math.floor(Math.random() * myArray.length)];
 }
 
-var Twitter = new twit(config);
 
 function MakeRandomText() {
-  var returnText = chooseRandom(dictionaries.starts) +
-    ' ' + chooseRandom(dictionaries.middles) +
-    ' ' + chooseRandom(dictionaries.endings)
-  if (returnText.length < 140) {
-    return returnText
-  } else {
-    return MakeRandomText()
-  }
+
 }
 
-//delete the previous tweet
-// Twitter.get('statuses/user_timeline', {
-//     screen_name: 'LogoffBot',
-//     count: 'numberOfSavedTweets'
+
+console.log(chooseRandom(deck.tarot_interpretations));
+
+// Twitter.post('statuses/update', {
+//     status: tweetText
 //   },
 //   function(err, data, response) {
 //     if (err) {
-//       console.log('error!' + err)
-//     } else if (data && data[(numberOfSavedTweets - 1)].id_str) { //check that there really was a tweet to delete
-//       var deleteID = data[(numberOfSavedTweets - 1)].id_str;
-//       Twitter.post('statuses/destroy/' + deleteID, {
-//           'id': deleteID
-//         },
-//         function(err, data, response) {
-//           if (err) {
-//             console.log(err)
-//           }
-//         })
-//     }
+//       console.log(err)
+//     };
 //   })
-
-var tweetText = MakeRandomText();
-console.log(tweetText);
-
-Twitter.post('statuses/update', {
-    status: tweetText
-  },
-  function(err, data, response) {
-    if (err) {
-      console.log(err)
-    };
-  })
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Lachesis: a tarot spread generator
+by Toby Fee
+
+## Credits
+Thanks to Darius K. for his amazing (corpora-project)[https://github.com/dariusk/corpora]
